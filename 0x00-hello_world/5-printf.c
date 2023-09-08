44d41d7c50b95e0dc1fa93d73538792d72e20016
Teju101 <adegokeadeteju001@gmail.com>
2023-09-07 20:57:34 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>
+/**
+ *  * main - Entry point
+ *   * print message
+ *    * Return: 0
+ *     */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
